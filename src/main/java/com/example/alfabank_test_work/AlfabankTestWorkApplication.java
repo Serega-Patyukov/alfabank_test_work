1cb667186e003203306f18c1715d97b4f6ec02d5
serega-patyukov <serega-patyukov@mail.ru>
2022-05-31 16:22:06 +0300

    Проект инициализирован

---
+++ src/main/java/com/example/alfabank_test_work/AlfabankTestWorkApplication.java
@@ -0,0 +1,13 @@
+package com.example.alfabank_test_work;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlfabankTestWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlfabankTestWorkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/alfabank_test_work/AlfabankTestWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.alfabank_test_work;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlfabankTestWorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
